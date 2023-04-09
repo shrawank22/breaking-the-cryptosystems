fb1f7d6d09e5f1fe7ae758ee57c145788b72b839
shrawank22 <shrawan8565@gmail.com>
2023-04-09 23:14:36 +0530

    initial commit

---
+++ cs641_a3/divide.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string c5 = "qmnjvsanvwewcflctvprjtjtvvplvlfvxjavqildhcxmlnvcnacyclpafcgytvfvwfvwgqyppqqpqcsywsqrxqmnjvafycgvtlvhfcwtylaeuqfvxjatkbvcqnsqslhfavawnccveasfuqbqvqtcyllrqrxxwacfypsdcuqfavrqcgefqpyattracxwvtaawwddveasflcbqvdtrawmvupqquwxdecgqcwtyqyaflvlqsyqklhqsnafqvmllhvqpawrnqgvfusrecwawyqpfnwgawdgf";
+	int l = c5.size(); // l=284
+	c5 += '*';
+	vector<string> vec;
+	string temp = "";
+	for (int i = 1; i < c5.size() + 1; i++) {
+		temp += c5[i - 1];
+		if (i != 0 && i % 5 == 0) {
+			vec.push_back(temp);
+			temp = "";
+		}
+	}
+
+
+	string res = "";
+	for (auto it : vec)
+		res += it + " ";
+	cout << res << endl;
+	return 0;
+}
